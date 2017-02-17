290d480a68227e9ed94d922e06751820a2eb130a
Todd Skinner <todd.skinner@gmail.com>
2017-02-16 23:48:08 -0800

    Add override to onOptionsItemSelected

--- app/src/main/java/com/example/android/moviesapp/DetailActivity.java
+++ app/src/main/java/com/example/android/moviesapp/DetailActivity.java
@@ -8,6 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
@@ -189,4 +190,12 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
